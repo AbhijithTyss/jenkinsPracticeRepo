66925d4dd7abd1ca910a8b00fab046424c2ad383
AbhijithTyss <abhijith.styss@gmail.com>
2024-06-13 09:56:56 +0530

    OnPollScmTest class added

---
+++ DemoJenkinsPractice/src/test/java/jenkins/OnPollScmTest.java
@@ -0,0 +1,11 @@
+package jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class OnPollScmTest {
+	@Test
+	public void onPollScm() {
+		Reporter.log("OnPollScmTest class executed",true);
+	}
+}
